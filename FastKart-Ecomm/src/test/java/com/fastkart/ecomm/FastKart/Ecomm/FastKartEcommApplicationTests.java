0168a70029ed185ab6e9f764040b0fa3848b490b
sagashukla <sagashukla@deloitte.com>
2023-07-27 11:33:57 +0530

    Setting up the springboot project

---
+++ FastKart-Ecomm/src/test/java/com/fastkart/ecomm/FastKart/Ecomm/FastKartEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastkart.ecomm.FastKart.Ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastKartEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
